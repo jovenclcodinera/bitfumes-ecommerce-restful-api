2de84e493a71532dec9221b2df8935812439cb4b
User <joven_deogracias@yahoo.com>
2020-05-18 22:32:35 +0800

    Created ReviewResource Transformer % Product All pagination

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,7 +19,7 @@ class ProductResource extends JsonResource
             'description'=>$this->detail,
             'price'=>$this->price,
             'stock'=>$this->stock > 0 ? $this->stock : 'Out of Stock',
-            'discount'=>$this->discount,
+            'discount'=>$this->discount . '%',
             'totalPrice'=>round($this->price - (($this->price * $this->discount / 100)), 2),
             'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No Rating Yet',
             'href'=> [
