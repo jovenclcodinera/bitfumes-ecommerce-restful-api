cff6b1be40c9e3e65405f4bbf83e82a52c078524
User <joven_deogracias@yahoo.com>
2020-05-18 22:05:39 +0800

    Modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock > 0 ? $this->stock : 'Out of Stock',
             'discount'=>$this->discount,
+            'totalPrice'=>round($this->price - (($this->price * $this->discount / 100)), 2),
+            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No Rating Yet',
+            'href'=> [
+                'reviews'=>route('reviews.index', $this->id)
+            ]
         ];
     }
 }
